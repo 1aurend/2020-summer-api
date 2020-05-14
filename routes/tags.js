67b7b6110ce3ce721994d23f5b066e109c2bd65e
Lauren Davidson <ldavidson@g.harvard.edu>
2020-05-14 14:20:51 -0700

    Add files for new routes

---
+++ routes/tags.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import Airtable from 'airtable'
+import { formatTagData } from '../utils/schemas'
+import { createRecord } from '../utils/promisifiedCreate'
+require('dotenv').config()
+
+const router = express.Router()
+const key = process.env.AT_KEY
+const devBase = new Airtable({apiKey: key}).base('appiLUFhewofNGI3D');
+const dupBase = new Airtable({apiKey: key}).base('appuSugdO02qPnW3D');
+
+
+router.get('/resourcetypes', (req, res) => {
+  
+})
+
+
+export default router
